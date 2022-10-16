151817eaaa0f5c8ab976d11c7d3bf73644601a3e
Mariana Lopez <joscelynn7u7@gmail.com>
2022-10-16 17:27:35 -0500

    add: Jetstream + Archivos Bootstrap
    Añadí el kit de Jetstream para tener una serie de funciones prediseñadas de PHP para mi aplicación.
    
    Añadí los recursos de css y js de la plantilla de Bootstrap que utilizaré para mi proyecto, asimismo, configuré vite para implementar el framework en mis vistas.

--- resources/views/commissions/commissionsIndex.blade.php
+++ resources/views/commissions/commissionsIndex.blade.php
@@ -5,9 +5,20 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Index</title>
+    @vite(['resources/css/bootstrap/bootstrap.css','resources/js/bootstrap/bootstrap.js'])
 </head>
 <body>
     <h1>Listado de Comisiones</h1>
+
+    <div class="card" style="width: 18rem;">
+    <div class="card-body">
+        <h5 class="card-title">Card title</h5>
+        <h6 class="card-subtitle mb-2 text-muted">Card subtitle</h6>
+        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+        <a href="#" class="card-link">Card link</a>
+        <a href="#" class="card-link">Another link</a>
+    </div>
+    </div>
     
     <a href="/commission/create">Crear nueva comisión</a>
 
